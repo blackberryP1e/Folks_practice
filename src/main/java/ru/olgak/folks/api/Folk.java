87ea56c8702ae286230437d9e371e69ae16716ce
Nazina Olga <olgak@hflabs.ru>
2017-09-19 19:57:51 +0300

    FOLKS-6 add lombok

--- src/main/java/ru/olgak/folks/api/Folk.java
+++ src/main/java/ru/olgak/folks/api/Folk.java
@@ -2,6 +2,9 @@ package ru.olgak.folks.api;
 
 import ru.olgak.folks.api.annotation.Searchable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
@@ -34,35 +37,4 @@ public class Folk extends AbstractEntity {
         this.devices = devices;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
 }
